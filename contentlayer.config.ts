c806ba5fa7f3c2e7188a593035b94f6c6ad1f458
Myos <myos.chen@gmail.com>
2023-05-14 00:21:08 +0800

    feat: handle markdown files with contentlayer

---
+++ contentlayer.config.ts
@@ -0,0 +1,32 @@
+import {defineDocumentType, makeSource} from 'contentlayer/source-files';
+
+export const Post = defineDocumentType(() => ({
+  name: 'Post',
+  filePathPattern: '**/*.md',
+  fields: {
+    title: {
+      type: 'string',
+      required: true,
+    },
+    description: {
+      type: 'string',
+      required: true,
+    },
+    slug: {
+      type: 'string',
+      required: true,
+    },
+    date: {
+      type: 'date',
+      required: true,
+    },
+  },
+  computedFields: {
+    url: {
+      type: 'string',
+      resolve: (post) => `/posts/${post.slug}`,
+    },
+  },
+}));
+
+export default makeSource({contentDirPath: 'content', documentTypes: [Post]});
