0f0434c12afcbb4e67d336e3688e2a498c3d75c5
Myos <myos.chen@gmail.com>
2023-05-13 23:28:25 +0800

    chore: add prettier, husky, commitlint, commitizen, lint-staged
    add prettier to format code, add husky to lint staged and check commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,17 @@
+// https://nextjs.org/docs/pages/building-your-application/configuring/eslint
+const path = require('path');
+
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+module.exports = {
+  '**/*.{js,jsx,ts,tsx}': [
+    buildEslintCommand,
+    'prettier --config ./.prettierrc.js --write',
+  ],
+  '**/*.{css,scss,md,html,json}': [
+    'prettier --config ./.prettierrc.js --write',
+  ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSameLine: false,
+  singleQuote: true,
+  bracketSpacing: false,
+  arrowParens: 'always',
+};
