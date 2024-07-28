c3f68cdb54c9b01981d4ea614da33a52a90e592c
Myos <myos.chen@gmail.com>
2024-07-28 17:14:41 +0800

    chore: support eslint v9

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,4 @@
+import { fixupPluginRules } from '@eslint/compat'
 import { FlatCompat } from '@eslint/eslintrc'
 import pluginNext from '@next/eslint-plugin-next'
 import stylistic from '@stylistic/eslint-plugin'
@@ -6,11 +7,10 @@ import parserTs from '@typescript-eslint/parser'
 import pluginReact from 'eslint-plugin-react'
 import pluginReactHooks from 'eslint-plugin-react-hooks'
 import pluginImportSort from 'eslint-plugin-simple-import-sort'
-import pluginUnusedImports from 'eslint-plugin-unused-imports'
 
 const compat = new FlatCompat()
 
-/** @type {import('eslint').Linter.FlatConfig} */
+/** @type {import('eslint').Linter.Config} */
 const style = stylistic.configs.customize({
   indent: 2,
   quotes: 'single',
@@ -19,7 +19,7 @@ const style = stylistic.configs.customize({
   braceStyle: '1tbs',
 })
 
-/** @type {import('eslint').Linter.FlatConfig} */
+/** @type {import('eslint').Linter.Config} */
 const next = {
   files: ['src/**/*.{ts,tsx}'],
   languageOptions: {
@@ -34,17 +34,20 @@ const next = {
     '@typescript-eslint': pluginTs,
     'react': pluginReact,
     'react-hooks': pluginReactHooks,
-    '@next/next': pluginNext,
+    '@next/next': fixupPluginRules(pluginNext),
   },
   rules: {
-    ...pluginTs.configs['eslint-recommended'].overrides[0].rules,
+    ...pluginTs.configs['eslint-recommended'].rules,
     ...pluginTs.configs['recommended'].rules,
     ...pluginReact.configs['jsx-runtime'].rules,
     ...pluginReactHooks.configs.recommended.rules,
     ...pluginNext.configs.recommended.rules,
     ...pluginNext.configs['core-web-vitals'].rules,
-    '@typescript-eslint/no-unused-vars': 'off',
-    '@stylistic/jsx-curly-brace-presence': ['error', 'always'],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
   },
 }
 
@@ -57,7 +60,7 @@ const tailwindcss = compat.config({
   },
 })
 
-/** @type {import('eslint').Linter.FlatConfig} */
+/** @type {import('eslint').Linter.Config} */
 const sort = {
   plugins: { 'import-sort': pluginImportSort },
   rules: {
@@ -78,21 +81,9 @@ const sort = {
   },
 }
 
-/** @type {import('eslint').Linter.FlatConfig} */
-const unused = {
-  plugins: { 'unused-imports': pluginUnusedImports },
-  rules: {
-    'unused-imports/no-unused-imports': 'error',
-    'unused-imports/no-unused-vars': [
-      'warn',
-      { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' },
-    ],
-  },
-}
-
-/** @type {import('eslint').Linter.FlatConfig} */
+/** @type {import('eslint').Linter.Config} */
 const ignores = {
   ignores: ['./.next/*'],
 }
 
-export default [style, next, ...tailwindcss, sort, unused, ignores]
+export default [style, next, ...tailwindcss, sort, ignores]
