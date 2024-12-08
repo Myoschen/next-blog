94f4fead2ca7f561278b1bb80e938269fea24bc6
Myoschen <myos.chen@gmail.com>
2024-12-08 16:17:19 +0800

    chore: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,4 @@
-import { fixupPluginRules } from '@eslint/compat'
-import { FlatCompat } from '@eslint/eslintrc'
+import { fixupConfigRules } from '@eslint/compat'
 import pluginNext from '@next/eslint-plugin-next'
 import stylistic from '@stylistic/eslint-plugin'
 import pluginTs from '@typescript-eslint/eslint-plugin'
@@ -7,83 +6,80 @@ import parserTs from '@typescript-eslint/parser'
 import pluginReact from 'eslint-plugin-react'
 import pluginReactHooks from 'eslint-plugin-react-hooks'
 import pluginImportSort from 'eslint-plugin-simple-import-sort'
+import pluginTailwind from 'eslint-plugin-tailwindcss'
 
-const compat = new FlatCompat()
-
-/** @type {import('eslint').Linter.Config} */
-const style = stylistic.configs.customize({
-  indent: 2,
-  quotes: 'single',
-  semi: false,
-  jsx: true,
-  braceStyle: '1tbs',
-})
-
-/** @type {import('eslint').Linter.Config} */
-const next = {
-  files: ['src/**/*.{ts,tsx}'],
-  languageOptions: {
-    parser: parserTs,
-    parserOptions: {
-      ecmaVersion: 'latest',
-      ecmaFeatures: { jsx: true },
-      sourceType: 'module',
-    },
+export default fixupConfigRules([
+  // ignores
+  {
+    ignores: ['node_modules', '.next', '.content-collections'],
   },
-  plugins: {
-    '@typescript-eslint': pluginTs,
-    'react': pluginReact,
-    'react-hooks': pluginReactHooks,
-    '@next/next': fixupPluginRules(pluginNext),
-  },
-  rules: {
-    ...pluginTs.configs['eslint-recommended'].rules,
-    ...pluginTs.configs['recommended'].rules,
-    ...pluginReact.configs['jsx-runtime'].rules,
-    ...pluginReactHooks.configs.recommended.rules,
-    ...pluginNext.configs.recommended.rules,
-    ...pluginNext.configs['core-web-vitals'].rules,
-  },
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-}
-
-const tailwindcss = compat.config({
-  extends: ['plugin:tailwindcss/recommended'],
-  settings: {
-    tailwindcss: {
-      callees: ['cn', 'cva'],
-    },
-  },
-})
-
-/** @type {import('eslint').Linter.Config} */
-const sort = {
-  plugins: { 'import-sort': pluginImportSort },
-  rules: {
-    'import-sort/exports': 'error',
-    'import-sort/imports': [
-      'error',
-      {
-        groups: [
-          ['^\\u0000'],
-          ['^react', '^next', '@?\\w'],
-          ['^~/.*'],
-          ['^@/.*'],
-          ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
-          ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
-        ],
+  // stylistic
+  stylistic.configs.customize({
+    indent: 2,
+    quotes: 'single',
+    semi: false,
+    jsx: true,
+    braceStyle: '1tbs',
+  }),
+  // react, next
+  {
+    files: ['src/**/*.{ts,tsx}'],
+    languageOptions: {
+      parser: parserTs,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
       },
-    ],
+    },
+    plugins: {
+      '@typescript-eslint': pluginTs,
+      'react': pluginReact,
+      'react-hooks': pluginReactHooks,
+      '@next/next': pluginNext,
+    },
+    rules: {
+      ...pluginTs.configs['eslint-recommended'].overrides[0].rules,
+      ...pluginTs.configs['recommended'].rules,
+      ...pluginReact.configs['jsx-runtime'].rules,
+      ...pluginReactHooks.configs['recommended'].rules,
+      ...pluginNext.configs['recommended'].rules,
+      ...pluginNext.configs['core-web-vitals'].rules,
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'warn',
+      '@typescript-eslint/no-require-imports': 'warn',
+    },
   },
-}
-
-/** @type {import('eslint').Linter.Config} */
-const ignores = {
-  ignores: ['./.next/*'],
-}
-
-export default [style, next, ...tailwindcss, sort, ignores]
+  // tailwindcss
+  ...pluginTailwind.configs['flat/recommended'],
+  {
+    settings: {
+      tailwindcss: {
+        callees: ['cn', 'cva'],
+        config: 'tailwind.config.ts',
+      },
+    },
+  },
+  // sort imports, exports
+  {
+    plugins: {
+      'import-sort': pluginImportSort,
+    },
+    rules: {
+      'import-sort/exports': 'error',
+      'import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^\\u0000'],
+            ['^react', '^next', '@?\\w'],
+            ['^./.*', '^~/.*', '^#/.*', '^@/.*'],
+            ['^\\.\\.(?!/?$)', '^\\.\\./?$'],
+            ['^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$'],
+          ],
+        },
+      ],
+    },
+  },
+])
